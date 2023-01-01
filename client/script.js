82c9d5d2e4cb29ee29d9d35ed4eed28b359385b5
CryptoBaba1 <cryptobaba15076@gmail.com>
2023-01-01 14:48:40 +0530

    Updated the Server

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from the Server
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://ai-chatter-service.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
